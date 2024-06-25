8e33d3b65ceb31891a12fa3805906f64e2f7a96b
zheng8102 <140306154+zheng8102@users.noreply.github.com>
2024-06-25 14:37:03 +0800

    Update airchain.sh

--- airchain.sh
+++ airchain.sh
@@ -134,7 +134,9 @@ if [ -f "$KEY_FILE" ]; then
     echo "文件 $KEY_FILE 已经存在，删除文件"
     rm -f "$KEY_FILE"
     # 执行创建密钥命令
-   echo "123" | eigenlayer operator keys create --key-type ecdsa --insecure wallet > output.txt
+   echo "123" | eigenlayer operator keys create --key-type ecdsa --insecure wallet
+   echo "123" | eigenlayer operator keys show wallet --key-type ecdsa --insecure --output json | jq -r '.public_key'
+   echo "123" | eigenlayer operator keys show wallet --key-type ecdsa --insecure --output json | jq -r '.address'
 else
     echo "文件 $KEY_FILE 不存在，执行创建密钥操作"
     # 执行创建密钥命令
