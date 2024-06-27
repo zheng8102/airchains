7c18e207ab7197c863c9c90ce4a66887a533eef8
zheng8102 <140306154+zheng8102@users.noreply.github.com>
2024-06-27 17:37:30 +0800

    Update airchain.sh

--- airchain.sh
+++ airchain.sh
@@ -149,7 +149,6 @@ go run cmd/main.go init \
     --stationAPI "http://127.0.0.1:1317" \
     --stationType "wasm"
 
-go run cmd/main.go keys junction --accountName wallet --accountPath $HOME/.tracks/junction-accounts/keys
 
 go run cmd/main.go prover v1WASM
 
